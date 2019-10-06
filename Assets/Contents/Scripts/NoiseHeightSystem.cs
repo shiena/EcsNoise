3e5ac81f2e9ab9e97295feed7bb6cb4358098053
KOGA Mitsuhiro <shiena.jp@gmail.com>
2019-10-06 14:30:29 +0900

    Add a NoiseHeightSystem

---
+++ Assets/Contents/Scripts/NoiseHeightSystem.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace Shiena.EcsNoise
+{
+	public class NoiseHeightSystem : ComponentSystem
+	{
+		protected override void OnCreate()
+		{
+			base.OnCreate();
+			GetEntityQuery(new EntityQueryDesc
+			{
+				All = new[] {ComponentType.ReadWrite<Translation>(),}
+			});
+		}
+
+		protected override void OnUpdate()
+		{
+			var time = Time.realtimeSinceStartup;
+			Entities.ForEach((ref Translation translation) =>
+				{
+					translation.Value.y = 3 * noise.snoise(new float2(time + 0.02f * translation.Value.x,
+						                      time + 0.02f * translation.Value.z));
+				});
+		}
+	}
+}
