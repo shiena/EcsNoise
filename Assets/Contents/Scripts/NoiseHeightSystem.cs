f8d6211f6d769012f70fb062d154d8e4726cebc9
KOGA Mitsuhiro <shiena.jp@gmail.com>
2019-10-06 14:47:21 +0900

    Convert from ComponentSystem to JobComponentSystem

--- Assets/Contents/Scripts/NoiseHeightSystem.cs
+++ Assets/Contents/Scripts/NoiseHeightSystem.cs
@@ -1,12 +1,24 @@
 ﻿using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
 
 namespace Shiena.EcsNoise
 {
-	public class NoiseHeightSystem : ComponentSystem
+	public class NoiseHeightSystem : JobComponentSystem
 	{
+		struct TranslationNoise : IJobForEach<Translation>
+		{
+			public float time;
+
+			public void Execute(ref Translation translation)
+			{
+				translation.Value.y = 3 * noise.snoise(new float2(time + 0.02f * translation.Value.x,
+					                      time + 0.02f * translation.Value.z));
+			}
+		}
+
 		protected override void OnCreate()
 		{
 			base.OnCreate();
@@ -16,14 +28,10 @@ namespace Shiena.EcsNoise
 			});
 		}
 
-		protected override void OnUpdate()
+		protected override JobHandle OnUpdate(JobHandle inputDeps)
 		{
-			var time = Time.realtimeSinceStartup;
-			Entities.ForEach((ref Translation translation) =>
-				{
-					translation.Value.y = 3 * noise.snoise(new float2(time + 0.02f * translation.Value.x,
-						                      time + 0.02f * translation.Value.z));
-				});
+			var job = new TranslationNoise {time = Time.realtimeSinceStartup};
+			return job.Schedule(this, inputDeps);
 		}
 	}
 }
